f6e712c9551f786a8e9c81043430987f813f3eed
Ben Spencer <dangerous.ben@gmail.com>
2011-08-06 15:54:18 +0200

    Simple file-based KVS.

---
+++ application/models/WalletStore.php
@@ -0,0 +1,29 @@
+<?php
+
+class WalletStore
+{
+    static public function get()
+    {
+        return new self;
+    }
+
+    public function save($id, $value)
+    {
+        file_put_contents($this->getPath($id), $value);
+    }
+
+    public function load($id)
+    {
+        return file_get_contents($this->getPath($id));
+    }
+
+    public function delete($id)
+    {
+        unlink($this->getPath($id));
+    }
+
+    protected function getPath($id)
+    {
+        return BASE_PATH . "/data/{$id}";
+    }
+}
