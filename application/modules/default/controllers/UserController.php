fe60787cd64d858a10b6584047eaec600fea29e3
Wayne Crouch <wcrouch@wc-fedora.(none)>
2011-08-06 13:59:16 +0100

    Committing changes before refresh

---
+++ application/modules/default/controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+class UserController extends Zend_Controller_Action
+{
+
+    public function indexAction()
+    {
+        $this->_redirect('dashboard');
+    }
+    
+    public function dashboardAction()
+    {
+        
+    }
+
+    public function dataAction()
+    {
+       
+      
+    }
+}
+
