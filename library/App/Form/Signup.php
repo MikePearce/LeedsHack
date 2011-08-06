d874f93e6df02a8c0177c7a4311e3be1d8a846fd
Mike Pearce <mike@mikepearce.net>
2011-08-06 22:40:44 +0100

    Removed fucking awful validator. Sorry ben.

--- library/App/Form/Signup.php
+++ library/App/Form/Signup.php
@@ -12,8 +12,6 @@ class App_Form_Signup extends Zend_Form
         $number = new Zend_Form_Element_Text('number');
         $number->setLabel('Your mobile number, dude...')
                 ->setRequired(true)
-                ->addValidator(new Zend_Validate_Int(), true)
-                ->addErrorMessage('This should be a phone number! (Dont add + or ())')
                 ->addValidator('NotEmpty', true);
 
         $submit = new Zend_Form_Element_Submit('submit');
