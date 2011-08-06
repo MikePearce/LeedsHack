503efa0a1c681af1a6f7d13bd68757cb45cd139f
Matt Williams <wattmilliams@gmail.com>
2011-08-06 15:33:07 +0100

    Simple POST listener which parses XML body

--- application/modules/default/controllers/IncomingController.php
+++ application/modules/default/controllers/IncomingController.php
@@ -4,26 +4,29 @@ class IncomingController extends Zend_Controller_Action
 	
 	public function init()
 	{	
+		$this->_helper->layout->disableLayout();
+		
+		$this->_helper->viewRenderer->setNoRender();
+		
 		if (!$this->_request->isPost()) {
 			throw new Zend_Http_Exception('Bad Request', 400);
 		}
 	}
 	
 	public function indexAction()
-	{	
+	{
+		$data = array();
+		
 		$reader = new XMLReader();
-		$reader->XML($this->_request->getPost())
-			   ->setParserProperty(XMLReader::VALIDATE, true);
+		$reader->XML($this->_request->getRawBody());
 		
-		if ($reader->isValid()) {
-			while ($reader->read()) {
-				
+		while($reader->read()) {
+			if ($reader->nodeType == XMLREADER::ELEMENT) {
+				$nodeIndex = $reader->localName;
+				$reader->read();
+				$data[$nodeIndex] = $reader->value;
 			}
-		} else {
-			throw new Zend_Http_Exception('Invalid ', 400);
 		}
-		
-		
 	}
 	
 }
